1900aefa4a8ffb38c52ec8e77a7d3ef6f81cc42d
dynamicwp team <dynamicwp team@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-14 04:15:52 +0000

    Adding first version of DynamicWP Image Cube
    git-svn-id: https://plugins.svn.wordpress.org/dynamicwp-image-cube/trunk@252338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dynamicwp-image-cube.php
@@ -0,0 +1,114 @@
+<?php
+/*
+Plugin Name: DynamicWP Image Cube
+Plugin URI: http://www.dynamicwp.net/plugins/free-plugin-dynamicwp-image-cube/
+Description: The Plugin will build your image gallery as cube face. The Cube rotates automatically to show your images. The plugin is based on: <a href="http://keith-wood.name/imageCube.html">keith-wood.name</a>
+Author: Reza Erauansyah
+Version: 1.0
+Author URI: http://www.dynamicwp.net
+*/
+
+// =============================== Cube Widget ======================================
+
+Class Image_Cube_Widget extends WP_Widget{
+
+	function Image_Cube_Widget(){
+		$control_ops = array( 'width' => 400, 'height' => 350, 'id_base' => 'image-cube-widget' );
+		$this->WP_Widget( 'image-cube-widget', 'DynamicWP Image Cube', $widget_ops, $control_ops );
+	}
+
+	function widget( $args, $instance ) {
+
+		$myid = $args['widget_id'];
+		$linkss = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)); 
+		echo "<script type=\"text/javascript\" charset=\"utf-8\" src=\"".$linkss."jquery.imagecube.js\"></script>";
+		echo "<script type=\"text/javascript\" charset=\"utf-8\">
+		/* <![CDATA[ */
+			var cuber = jQuery.noConflict();
+			cuber(function () {
+				cuber('#basicCube-".$myid."').imagecube();
+			});
+		/* ]]> */
+		</script>";
+ 
+		extract( $args );
+		$height = ($instance['height']) ? $instance['height'] : 100 ;
+		$width = ($instance['width']) ? $instance['width'] : 200;
+		$title = apply_filters('widget_title', $instance['title'] );
+
+		echo $before_widget;
+		if($title) echo $before_title . $title . $after_title;
+		  echo "<div id='basicCube-".$myid."' style='width:".$width."px; height:".$height."px;'>";
+			 $option = $instance['image_link'];
+			 if($option) {
+				 $values = explode(",", $option);
+				 if(is_array($values)) {
+					foreach ($values as $item) {
+						if(!empty($item)) {
+							echo "<img src=\"$item\" alt=\"img\" title=\"\" /> \n";
+						}
+					 }
+				 }
+			 }
+		  echo "</div><br /><div style='clear: both;'></div><span style='float: right; font-size: 9px;'>widget by <a href='http://www.dynamicwp.net' target='_blank'>DynamicWP</a></span><div style='clear:both;'></div>";
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['width'] = strip_tags( $new_instance['width'] );
+		$instance['height'] = strip_tags( $new_instance['height'] );
+		$instance['image_link'] = strip_tags( $new_instance['image_link'] );
+		return $instance;
+	}
+
+	function form($instance){?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>">Title</label>
+			<input id="<?php echo $this->get_field_id('title'); ?>"
+				name="<?php echo $this->get_field_name('title'); ?>"
+				value="<?php echo $instance['title']; ?>"
+				class="widefat"
+			/>
+			<label for="<?php echo $this->get_field_id('width'); ?>">Image Width</label>
+			<input id="<?php echo $this->get_field_id('width'); ?>"
+				name="<?php echo $this->get_field_name('width'); ?>"
+				value="<?php echo $instance['width']; ?>"
+				class="widefat"
+			/>
+			<label for="<?php echo $this->get_field_id('height'); ?>">Image Height</label>
+			<input id="<?php echo $this->get_field_id('height'); ?>"
+				name="<?php echo $this->get_field_name('height'); ?>"
+				value="<?php echo $instance['height']; ?>"
+				class="widefat"
+			/>
+			<label for="<?php echo $this->get_field_id('image_link'); ?>">Image Link : (enter your full image links here. Separate the images with comma ',') </label>
+			<textarea id="<?php echo $this->get_field_id('image_link'); ?>"
+				name="<?php echo $this->get_field_name('image_link'); ?>" class="widefat" rows="10"><?php echo $instance['image_link']; ?></textarea>
+
+		</p>	
+	<?php }
+	
+
+
+}
+
+function mypunccube(){
+	if( !is_admin()){
+	   wp_deregister_script('jquery');
+	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"), false, '');
+	   wp_enqueue_script('jquery');
+	}
+}
+
+if(!is_admin()){
+   add_action('wp_head', 'mypunccube', 1);
+}
+
+// get WP to load our widget
+function init_image_cube(){
+	register_widget('Image_Cube_Widget');
+}
+add_action("widgets_init", "init_image_cube");
+?>
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== DynamicWP Image Cube ===
+Contributors: dynamicwp team
+Donate link: http://dynamicwp.net
+Tags: image, cube, gallery, widget, sidebar
+Requires at least: 2.8
+Tested up to: 2.9.2
+Stable tag: 1.0
+
+This plugin will build your image gallery as cube face. And the cube will random rotates automatically to show your images.
+
+== Description ==
+
+DynamicWP image cube is a jQuery plugin that will build your image gallery as cube face. And the cube will random rotates automatically to show your images. This plugin is a widget, so you can put it anywhere you like on your sidebar.
+
+**Features**
+
+* Easy to use
+* No flash needed
+* Support .jpg and .png image
+* Unlimited image to show
+* Automatic crop image
+
+**Demo**
+
+Please check [the plugin demo page](http://www.dynamicwp.net/image-cube-demo-page).
+
+**Credit** 
+
+This plugin based [The jQuery image cube](http://keith-wood.name/imageCube.html), author Keith Wood
+
+== Installation ==
+
+1. Decompress the .zip archive and put the “Dynamicwp-Image-Cube” category into your plugins directory (/wp-content/plugins/).
+1. Or the other way, with your wordpress admin page > Plugins > Add New then choose upload on the top. Next choose File (.zip) then Install Now. (see the screenshots)
+1. Activate the plugin in the WordPress Plugins admin page.
+1. Now you can see a new widget "DynamicWP Image Cube" in your widget page. (see the screenshots)
+1. Drag "DynamicWP Image Cube" to your sidebar area and fill the title, image width, image height and image link box, use full path link, and separate between links with comma. (see the screenshots)
+
+== Screenshots ==
+
+1. Image galleries as cube face on sidebar
+2. Plugin installation using WordPress admin panel
+3. "DynamicWP Image Cube" in the widget page
+4. How to insert the links
+
+== Other Notes ==
+
+You can setting dimension of the image in widget settings page (Appearance > Widgets). The default dimension is 200x100 px.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
